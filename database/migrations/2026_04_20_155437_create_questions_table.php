8de8ef4325a417a1ca3cee5cbbdd48ca8185ca37
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2026-04-22 04:04:06 +0700

    Add question_type column to questions table

--- database/migrations/2026_04_20_155437_create_questions_table.php
+++ database/migrations/2026_04_20_155437_create_questions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('quiz_id')->constrained()->cascadeOnDelete();
             $table->text('question_text');
+            $table->enum('question_type', ['PG', 'IS', 'AU']);
             $table->string('audio_path')->nullable();
             $table->timestamps();
         });
